57b164351309d35eb7bec8b158af30709ae55f92
Chribtoper <xcstolfax@gmail.com>
2018-09-09 22:54:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do [x]
+    puts x
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
