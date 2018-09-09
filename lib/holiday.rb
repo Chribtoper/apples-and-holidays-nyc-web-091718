da5aa70dc4c988237bfd3de95d5e46ad12858d8f
Chribtoper <xcstolfax@gmail.com>
2018-09-09 23:00:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,8 +40,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
    puts "#{season.capitalize}:"
-   holiday.each do |holiday, item|
-     puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+   holiday.each do |holiday, value|
+     puts "  #{holiday.to_s.split("_").map {|x| x.capitalize}.join(" ")}: #{value.join(", ")}"
    end
  end
 end
